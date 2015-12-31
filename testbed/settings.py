d7f6ef5bdcc77348e2535d4bdc4c586501fd1b60
Mark Hamilton <mark_lee_hamilton@att.net>
2015-12-30 17:58:58 -0800

    changed testdbsite to db

--- testbed/settings.py
+++ testbed/settings.py
@@ -23,7 +23,7 @@ import os
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 ##
 # Location of the test database root directory.
-TEST_DBSITE_DIR = os.path.abspath(os.path.join(BASE_DIR, "testdbsite"))
+TEST_DBSITE_DIR = os.path.abspath(os.path.join(BASE_DIR, "db"))
 
 PLUGINS = {
     'testbed.libexec',
